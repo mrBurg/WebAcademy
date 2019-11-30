a7ca14e79d4ab670f5b3d85f89abfa561b002835
Fedor Burgonov <fed-@ukr.net>
2019-12-01 00:27:34 +0200

    Home Work 2 Uploaded

---
+++ Lessons/Unit2/hw/clock/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import styles from './App.module.scss';
+import styles from './index.module.scss';
+
+function App() {
+  return (
+    <header className={ styles.header }>
+      lorem
+    </header>
+  );
+}
+
+export default App;
